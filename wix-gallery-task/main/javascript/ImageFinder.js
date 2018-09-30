38b8d4cd44b494e99346c38e8c6d55098295f1a7
TheBestDon <donatas_bakanas@yahoo.com>
2018-09-30 18:25:12 +0300

    static search moved to seperate file

--- wix-gallery-task/main/javascript/ImageFinder.js
+++ wix-gallery-task/main/javascript/ImageFinder.js
@@ -1,24 +1,5 @@
 (function() {
   var ImageFinder = (window.CLASSES.ImageFinder = function() {});
 
-  ImageFinder.prototype.search = function(query) {
-    const pictures = window.DATA.staticImagesDb;
-    const images = [];
-
-    const filteredPictures = pictures.filter(picture => {
-      return picture.title.toLowerCase().indexOf(query.toLowerCase()) > -1;
-    });
-
-    filteredPictures.forEach(picture => {
-      return images.push({
-        id: picture.id,
-        url: picture.url,
-        title: picture.title
-      });
-    });
-    return {
-      query,
-      images
-    };
-  };
+  ImageFinder.prototype.search = function(query) {};
 })();
