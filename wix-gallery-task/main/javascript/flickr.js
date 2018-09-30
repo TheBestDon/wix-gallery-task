3ef5c1c7b3c1841bbac5900857357c11a99225b7
TheBestDon <donatas_bakanas@yahoo.com>
2018-09-30 18:40:04 +0300

    flickr api search function created

---
+++ wix-gallery-task/main/javascript/flickr.js
@@ -0,0 +1,28 @@
+window.MODULES.Flickr = async function(query) {
+  const opts = {
+    api_key: 'b394136d5dde8d9d0d4f8fc6685386e2'
+  };
+  const photos = await window.fetch(
+    `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
+      opts.api_key
+    }&text=${query}&format=json&nojsoncallback=1
+    `
+  );
+  const data = await photos.json();
+  const pictures = data.photos.photo;
+  const images = [];
+
+  pictures.forEach(picture => {
+    return images.push({
+      id: picture.id,
+      url: `https://farm${picture.farm}.staticflickr.com/${picture.server}/${
+        picture.id
+      }_${picture.secret}_s.jpg`,
+      title: picture.title
+    });
+  });
+  return {
+    query,
+    images
+  };
+};
